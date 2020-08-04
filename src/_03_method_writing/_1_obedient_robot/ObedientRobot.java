71104f4fb966908b7b8c7a1e20bec25ff4cd7e49
MegaMiles <55776997+MegaMiles@users.noreply.github.com>
2020-08-03 20:10:27 -0700

    ObedientRobot.java

--- src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
+++ src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
@@ -9,10 +9,11 @@ public class ObedientRobot {
 	
 public static void main(String[] args) {
 	bob.penDown();
+	bob.setSpeed(1000000000);
 String Shape = JOptionPane.showInputDialog(null,"What shape do you want the robot to draw?"
 		+ " A Square,Triangle, or Circle");
 
-String color = JOptionPane.showInputDialog(null,"what color do you want the robot to draw in?"
+String color = JOptionPane.showInputDialog(null,"what color do you want the robot to draw in? "
 		+ "Red, yellow, green, or blue");
 if (color.equalsIgnoreCase("Red")) {
 	bob.setPenColor(Color.RED);
@@ -35,12 +36,19 @@ if (Shape.equalsIgnoreCase("Triangle")) {
 if (Shape.equalsIgnoreCase("Circle")) {
 	drawCircle();
 }
+if (Shape.equalsIgnoreCase("Mia")) {
+	drawDog();
+}
+if (Shape.equalsIgnoreCase("Alissa")) {
+drawHexagon();	
+}
 }
 static void drawSquare() {
 bob.move(100);	
 bob.turn(90);
 bob.move(100);
-bob.turn(90);	
+bob.turn(90);
+bob.move(100000);
 }
 static void drawTriangle() {
 bob.move(100);
@@ -49,6 +57,7 @@ bob.move(100);
 bob.turn(60);
 bob.move(100);
 bob.turn(60);
+bob.move(10000000);
 }
 static void drawCircle() {
 for (int i = 1; i < 360; i++) {
@@ -56,5 +65,44 @@ bob.move(2);
 bob.turn(1);
 }	
 }
+static void drawDog() {
+bob.penUp();
+bob.move(20);
+bob.penDown();
+bob.move(30);
+bob.turn(90);	
+bob.move(20);	
+bob.turn(-60);	
+bob.move(30);
+bob.turn(120);
+bob.move(30);
+bob.turn(-40);
+bob.move(100);
+bob.turn(160);
+bob.move(145);
+bob.turn(180);
+bob.move(70);
+bob.turn(-90);
+bob.penUp();
+bob.move(20);
+bob.penDown();
+bob.move(1);
+bob.penUp();
+bob.move(100000);
+}
+static void drawHexagon(){
+
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
 
 }
