133e67f06d52ce44c400a2fd1bf5303b8ba17936
MegaMiles <55776997+MegaMiles@users.noreply.github.com>
2020-08-05 11:28:47 -0700

    HappyPet.java

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,10 +5,11 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-static int happinessLevel = 0;	
+static int happinessLevel = 0;
+static String petName;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-String petName = JOptionPane.showInputDialog(null,"What Pet do you want");
+		 petName = JOptionPane.showInputDialog(null,"What Pet do you want");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
@@ -18,33 +19,45 @@ String petName = JOptionPane.showInputDialog(null,"What Pet do you want");
 					new String[] { "Or feed it", "Take it on a walk", "Cuddle it", "Pet it" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-
+			System.out.println(task);
+			if(task==3) {
+petIt();
+}			
+			if(task==2) {
+cuddleIt();
+}
+			if(task==1) {
+takeonWalk();
+}
+			if(task==0) {
+feedIt();
+}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+if (happinessLevel == 10) {
+	JOptionPane.showMessageDialog(null, "You definitly love your " + petName + " a lot");
+}
 	
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
-System.out.println(task);
-			if(task==3) {
-JOptionPane.showMessageDialog(null, "The " + petName + " likes your pet");
-happinessLevel+= 2;
-}			
-			if(task==2) {
-JOptionPane.showMessageDialog(null, "");				
+
 }
-			if(task==1) {
-JOptionPane.showMessageDialog(null, "");			
+	static void petIt() {	
+		JOptionPane.showMessageDialog(null, "The " + petName + " likes you peting it");
+		happinessLevel+= 2;
+	}
+	static void cuddleIt() {	
+		JOptionPane.showMessageDialog(null, "Your " + petName + " loves your cuddle and wants you to do it more");				
+		happinessLevel+= 3;;
+	}
+	static void takeonWalk() {	
+		JOptionPane.showMessageDialog(null, "Your " + petName + " liked the walk but it was tired afterward");			
+		happinessLevel+= 2;
+	}
+	static void feedIt() {	
+		JOptionPane.showMessageDialog(null, "Your " + petName + " loved you feeding it, it wants you to do it again");			
+		happinessLevel+= 4;
+	}
 }
-			if(task==0) {
-JOptionPane.showMessageDialog(null, "");			
-}
-
-
-
-
-
-
-}}
